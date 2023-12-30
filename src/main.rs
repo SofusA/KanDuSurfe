01a0bba160d90eef2d7f1a50802d735e5d3cc161
Sofus Addington <sofus@addington.dk>
2023-12-30 18:18:21 +0100

    container

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use axum::{routing::get, Router};
-use std::env;
 use std::net::SocketAddr;
 
 use handler_lib::functions::get_response::get_response;
@@ -8,13 +7,7 @@ use handler_lib::functions::get_response::get_response;
 async fn main() {
     let app = Router::new().route("/api/v1", get(get_response));
 
-    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
-    let port: u16 = match env::var(port_key) {
-        Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
-        Err(_) => 3000,
-    };
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5000));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
